bef97dbfdf41ab3b36a783255cf71e458f871f46
Jag9975 <49300175+Jag9975@users.noreply.github.com>
2021-01-23 15:23:35 -0800

    fixed search bar and added fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 import "./App.css";
+//import { Button } from 'reactstrap';
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
     </div>
+    /*<div>
+      <Card />
+    </div> */
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,53 @@ import SearchIcon from "@material-ui/icons/Search";
 
 function Navbar() {
   const [showLinks, setShowLinks] = useState(false);
+  const [data, setdata] = useState("");
+  const [results, setresults] = useState([]);
+
+  async function fetchdata() {
+    try {
+      fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?query=${data}&apiKey=5d3e34bc4dd4492485f19a63dff768fd`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          //return error if code: 402
+          console.log(res);
+          setresults(res.results);
+          console.log(results);
+        });
+    } catch (e) {}
+  }
+
   return (
-    <div className="Navbar">
-      <div className="leftSide">
-        <div className="links" id={showLinks ? "hidden" : ""}>
-          <a href="/home" className="webTitle">
-            Easy Recipes
-          </a>
-          <a href="/home">Home</a>
-          <a href="/view">View</a>
-          <a href="/post">Post</a>
-          <a href="/forum">Forum</a>
+    <div>
+      <div className="Navbar">
+        <div className="leftSide">
+          <div className="links" id={showLinks ? "hidden" : ""}>
+            <a href="/home" className="webTitle">
+              Easy Recipes
+            </a>
+            <a href="/home">Home</a>
+            <a href="/view">View</a>
+            <a href="/post">Post</a>
+            <a href="/forum">Forum</a>
+          </div>
+          <button onClick={() => setShowLinks(!showLinks)}>
+            <ReorderIcon />
+          </button>
         </div>
-        <button onClick={() => setShowLinks(!showLinks)}>
-          <ReorderIcon />
-        </button>
+        <div className="rightSide">
+          <input
+            onChange={(event) => setdata(event.target.value)}
+            type="text"
+          />
+          <button className="searchBar" onClick={(e) => fetchdata(e)}>
+            <SearchIcon />
+          </button>
+        </div>
       </div>
-      <div className="rightSide">
-        <input type="text" placeholder="Search" />
-        <button className="searchBar">
-          <SearchIcon />
-        </button>
+      <div id="datafield">
+        <p> {data} </p>
       </div>
     </div>
   );
